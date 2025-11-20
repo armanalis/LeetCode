8abdb1de1b4fa51147bf096d74ce74c18e609f0e
Ali <s309299@studenti.polito.it>
2025-11-20 09:28:32 +0100

    Done.

--- 9.palindrome-number.c
+++ 9.palindrome-number.c
@@ -2,7 +2,27 @@
  * @lc app=leetcode id=9 lang=c
  *
  * [9] Palindrome Number
- */
+ 
+ The problem:
+ Given an integer x, return true if x is a palindrome, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.*/
 
 // @lc code=start
 bool isPalindrome(int x) {
