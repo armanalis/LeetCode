483b2bba6d181d1a570de3c28cb036ecb8c12d02
Ali <s309299@studenti.polito.it>
2025-11-24 10:26:25 +0100

    Done.

--- 20.valid-parentheses.c
+++ 20.valid-parentheses.c
@@ -5,8 +5,26 @@
  */
 
 // @lc code=start
+#include <string.h>
 bool isValid(char* s) {
-    
+    //LAST IN FIRST OUT
+    int n = strlen(s);
+    char stack[n];
+    int top = -1;
+    for (int i = 0; i < n; i++){
+        if (s[i] == '(' || s[i] == '{' || s[i] == '['){
+            stack[++top] = s[i];
+        }
+        else {
+            if (top == -1) return false;    
+            char last = stack[top--];
+            int diff = s[i] - last;
+            if (diff != 1 && diff != 2){
+                return false;
+            }
+        }
+    }
+    return top == -1;
 }
 // @lc code=end
 
