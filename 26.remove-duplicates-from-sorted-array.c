1ee98ad0c8c19bba4d94172be868fa2fa068f7dd
Ali <s309299@studenti.polito.it>
2025-11-26 18:28:48 +0100

    Done.

---
+++ 26.remove-duplicates-from-sorted-array.c
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=26 lang=c
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+int removeDuplicates(int* nums, int numsSize) {
+    int k = 0;
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] != nums[k]) {//i = 2
+            k++;//k = 1
+            nums[k] = nums[i];
+        }
+
+    }
+    return k + 1;
+        
+}
+// @lc code=end
+
