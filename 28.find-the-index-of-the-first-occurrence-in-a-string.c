a61239eec51cca72314b27499116d0dd842e0fbe
Ali <s309299@studenti.polito.it>
2025-11-29 13:49:26 +0100

    Done.

--- 28.find-the-index-of-the-first-occurrence-in-a-string.c
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.c
@@ -5,8 +5,23 @@
  */
 
 // @lc code=start
+#include <string.h>
 int strStr(char* haystack, char* needle) {
-    
+    int i, j;
+    int L_haystack = strlen(haystack);
+    int L_needle = strlen(needle);
+
+    for(i = 0; i <= L_haystack - L_needle; i++){
+        for(j = 0; j < L_needle; j++){
+            if(haystack[i+j] != needle[j]){
+                break;
+            }
+        }
+        if(j == strlen(needle)){
+            return i;
+        }
+    }
+    return -1;
 }
 // @lc code=end
 
