351fa7856ad14655a35b4917e8cad6ed874ce4b7
Ali <s309299@studenti.polito.it>
2025-11-30 08:34:30 +0100

    Created.

---
+++ 66.plus-one.c
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=66 lang=c
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    
+}
+// @lc code=end
+
