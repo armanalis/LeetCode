340b17f8e6f9f3473bbcc10b8cc19b7959e8de95
Ali <s309299@studenti.polito.it>
2025-12-12 23:52:02 +0100

    Done.

--- 100.same-tree.c
+++ 100.same-tree.c
@@ -14,7 +14,13 @@
  * };
  */
 bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
-    
+    if (p == NULL && q == NULL) {
+        return true;
+    }
+    if (p == NULL || q == NULL || p->val != q->val) {
+        return false;
+    }
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
 }
 // @lc code=end
 
