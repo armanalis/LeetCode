eeb04bcf80455b9da8fd10659f7fa1c319a5a9b4
Ali <s309299@studenti.polito.it>
2025-12-17 17:35:29 +0100

    Done.

---
+++ 110.balanced-binary-tree.c
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=110 lang=c
+ *
+ * [110] Balanced Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isBalanced(struct TreeNode* root) {
+    
+}
+// @lc code=end
+
