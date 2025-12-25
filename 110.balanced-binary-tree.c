3a665a0119ee78e963e05365a0373c46491ffa89
Ali <s309299@studenti.polito.it>
2025-12-25 06:51:50 +0100

    Done.

--- 110.balanced-binary-tree.c
+++ 110.balanced-binary-tree.c
@@ -13,8 +13,39 @@
  *     struct TreeNode *right;
  * };
  */
+#include <stdbool.h>
+
+int checkheight(struct TreeNode* root);
 bool isBalanced(struct TreeNode* root) {
-    
+    if(checkheight(root) == -1){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+int checkheight(struct TreeNode* node){
+    if(node == NULL){
+        return 0;
+    }
+    int lheight = checkheight(node->left);
+    if(lheight == -1){
+        return -1;
+    }
+
+    int rheight = checkheight(node->right);
+    if(rheight == -1){
+        return -1;
+    }
+    int maxH = (lheight > rheight) ? lheight : rheight;
+    if(lheight - rheight > 1 || rheight - lheight > 1){
+        return -1;
+    }
+    else{
+        return 1 + maxH;
+    }
+
 }
 // @lc code=end
 
