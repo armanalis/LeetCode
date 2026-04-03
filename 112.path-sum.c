35d356c475c2fda683c32a9c33b6157aeff456a9
Ali <s309299@studenti.polito.it>
2026-04-03 11:23:52 +0200

    Done.

---
+++ 112.path-sum.c
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=112 lang=c
+ *
+ * [112] Path Sum
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool hasPathSum(struct TreeNode* root, int targetSum) {
+
+    if(root == NULL){
+        return false;
+    }
+
+    if(root->left == NULL && root->right == NULL){
+        if(root->val == targetSum){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+
+}
+// @lc code=end
+
